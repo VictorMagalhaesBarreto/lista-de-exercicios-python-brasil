6bbaa7cdbe10845c4779d5339c49518eeaad0e29
Victor Magalhaes <victor.barreto@buser.com.br>
2022-05-25 15:15:19 -0300

    Seção 2 parte 1

--- secao_02_estrutura_de_decisao/ex_04_vogal_ou_consoante.py
+++ secao_02_estrutura_de_decisao/ex_04_vogal_ou_consoante.py
@@ -17,3 +17,8 @@ Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
 
 def vogal_ou_consoante(letra):
     """Escreva aqui em baixo a sua solução"""
+
+    if letra=='A' or letra=='a' or letra=='E' or letra=='e' or letra=='I' or letra=='i' or letra=='O' or letra=='o' or letra=='U' or letra=='u':
+        print("'vogal'")
+    else:
+        print("'consoante'")
--- secao_02_estrutura_de_decisao/ex_08_escolha_de_produto.py
+++ secao_02_estrutura_de_decisao/ex_08_escolha_de_produto.py
@@ -20,3 +20,4 @@ Mostrar o resultado com duas casas decimais
 
 def decidir_melhor_produto(x, y, z):
     """Escreva aqui em baixo a sua solução"""
+
